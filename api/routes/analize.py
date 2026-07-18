912e56292c78a57151c1bd23601cd057dbe48472
= <=>
2026-07-18 16:00:27 +0530

    (fix): Made more changes in UI style , Icon , Theme seelection, Multlanguage Support.

--- api/routes/analize.py
+++ api/routes/analize.py
@@ -3,15 +3,18 @@ from util import get_model,get_vectorization
 from schemas.analize_schema import AnalyzeSchema
 from schemas.return_schema import ReturnResponse
 from services.agent_service import ai_explaination
+from util import info,success
 
 router = APIRouter()
 
 
 @router.post("")
 def analize(payload:AnalyzeSchema,model = Depends(get_model),vectorizor = Depends(get_vectorization)):
+        info("Incoming request...")
         vector_message = vectorizor.transform([payload.message])
         prediction = model.predict(vector_message)[0]
-
+        info("SVM Prediction done")
+    
         ai_explain = ai_explaination(
             payload.message,
             payload.sender,
@@ -19,6 +22,8 @@ def analize(payload:AnalyzeSchema,model = Depends(get_model),vectorizor = Depend
             payload.language
         )
 
+        info("AI Explanation done.")
+        success("Returning the result.")
         return ReturnResponse(
             status_code=200,
             status=True,
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import joblib
+from loguru import logger
 
 svm_model = joblib.load('models/spam_classifier.pkl')
 vectorizor = joblib.load('models/vectorizer.pkl')
@@ -9,3 +10,19 @@ def get_model():
 
 def get_vectorization():
     return vectorizor
+
+
+def info(message:str):
+    logger.info(message)
+
+def success(message:str):
+    logger.success(message)
+
+def debug(message:str):
+    logger.debug(message)
+
+def error(message:str):
+    logger.error(message)
+
+def warning(message:str):
+    logger.warning(message)
